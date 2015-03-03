9ab68c561b0a00ade0bb2d3d17bcacd4e9f99f75
Shaun Duncan <shaun.duncan@gmail.com>
2015-03-02 23:03:24 -0500

    Initial commit

---
+++ README.rst
@@ -0,0 +1,18 @@
+helga-wiki-whois
+================
+
+A helga command plugins that shows a wiki/intranet page page for a user.
+
+Settings
+--------
+
+**WIKI_URL** Formattable WIKI url containing the substring named {user} (i.e. http://example.com/~{user})
+
+License
+-------
+
+Copyright (c) 2015 Shaun Duncan
+
+Licensed under an `MIT`_ license.
+
+.. _`MIT`: https://github.com/shaunduncan/helga-wiki-whois/blob/master/LICENSE
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='helga-no-more-olga',
+    version='0.0.1',
+    description='A helga matcher that tells people to talk to helga instead of olga',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Communications :: Chat :: Internet Relay Chat',
+        'Framework :: Twisted',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords='helga olga',
+    author="Shaun Duncan",
+    author_email="shaun.duncan@gmail.com",
+    url="https://github.com/shaunduncan/helga-no-more-olga",
+    packages=find_packages(),
+    py_modules=['helga_no_more_olga'],
+    include_package_data=True,
+    zip_safe=True,
+    entry_points=dict(
+        helga_plugins=[
+            'no_more_olga = helga_no_more_olga:no_more_olga',
+        ],
+    ),
+)
